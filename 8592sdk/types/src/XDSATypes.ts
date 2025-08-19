07496f862dc123fd14b672f84b037eedefa370a1
Aryan Tadepalli <79216214+g5works@users.noreply.github.com>
2025-08-19 14:20:23 -0500

    add descriptions for types

--- 8592sdk/types/src/XDSATypes.ts
+++ 8592sdk/types/src/XDSATypes.ts
@@ -1,19 +1,19 @@
 
 export enum XDSAPresentationRequest {
-    BASIC,
-    ENUMERATION,
-    SLIDER,
-    COUNTER,
-    SPECIAL
+    BASIC, // how the thing is rendered when presentationrequest is omitted
+    ENUMERATION, // buttons for each type (number specific)
+    SLIDER, // stepped slider
+    COUNTER, // + & - buttons for inc/dec
+    SPECIAL // this is whatever you want it to be, and is unused.
 }
 
 export enum XDSACategory {
-    GENERAL,
-    AUTONOMOUS,
-    TELEOP,
-    ENDGAME,
-    DRIVER,
-    OTHER
+    GENERAL, //stuff like scouter/team name, etc
+    AUTONOMOUS, //auto scoring
+    TELEOP, // teleop scoring
+    ENDGAME, //endgame scoring/performance
+    DRIVER, //driver ability
+    OTHER //random stuff, like comments, or stuff that couldnt easily fit into the other 5 categories. use this sparingly.
 }
 
 export enum XDSAType {
@@ -56,17 +56,17 @@ export interface XDSADatabase {
 
 export interface XDSADatapoint {
 
-    name: string,
-    presentableName: string,
-    category: XDSACategory,
-    type: XDSAType
-    requested_presentation?: XDSAPresentationRequest,
-    gamepiece?: XDSAGamePiece, 
-    default?: any,
-    unique?: boolean,
-    maximum?: number,
-    minimum?: number,
-    enumValues?: {[val: number]: XDSAEnumConfiguration},
+    name: string, // the internal name of the datapoint for the db
+    presentableName: string, //the name on the ui
+    category: XDSACategory, //the presentation category, like general stuff, or teleop specific stuff
+    type: XDSAType // the type of the datapoint (number, string, etc)
+    requested_presentation?: XDSAPresentationRequest, //the way the point should be presented, really only for numerical stuff, such as sliders or counters.
+    gamepiece?: XDSAGamePiece, //the game piece which this datapoint is coupled to. generally only useful for counters.
+    default?: any, // the default value 
+    unique?: boolean, // is this value unique (this should generally always be false)
+    maximum?: number, // the max value (for sliders/counters)
+    minimum?: number, // the minimum value (for sliders/counters)
+    enumValues?: {[val: number]: XDSAEnumConfiguration}, // the names of each position in a slider or enum button (only works for these presentations only)
 
 }
 
